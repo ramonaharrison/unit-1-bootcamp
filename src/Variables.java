9eb826ef5c53c5eb993f9f0573bf954b35f6fb60
nuily <mslinhui@gmail.com>
2016-12-25 01:02:10 -0500

    complete

--- src/Variables.java
+++ src/Variables.java
@@ -2,8 +2,29 @@
  * Created by alexandraqin on 4/14/15.
  */
 public class Variables {
+    private int roomNum = 113;
+    private double eRounded = 2.71828;
+    private String compSci = "Computer Science";
+    private String HELLO_WORLD = "Hello World";
 
-  public static void main (String args[]) {
+    private boolean isOdd(int number) {
+        return (number % 2 != 0);
+    }
 
-  }
+    private void printHelloWorld(String HELLO_WORLD) {
+        System.out.println(HELLO_WORLD);
+    }
+
+    private void greatestCommonFactor(int a, int b) {
+
+    }
+
+
+    public static void main(String args[]) {
+        Variables variables = new Variables();
+        System.out.println("This is room # " + variables.roomNum);
+        System.out.println("e is close to " + variables.eRounded);
+        System.out.println("I am learning a bit about " + variables.compSci);
+
+    }
 }
